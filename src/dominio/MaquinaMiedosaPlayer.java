d08c28643a74b584f0d688088e84b21ba47367a9
YeltzynS <yeltcolo@gmail.com>
2023-12-09 18:25:24 -0500

    Update MaquinaMiedosaPlayer.java

--- src/dominio/MaquinaMiedosaPlayer.java
+++ src/dominio/MaquinaMiedosaPlayer.java
@@ -5,8 +5,32 @@ public class MaquinaMiedosaPlayer extends Player {
     public MaquinaMiedosaPlayer(String nombre, String color) {
         super(nombre, color);
     }
+    public Celda[][] getBoard() {
+
+        return Gomoku.getInstance().getBoard();
+
+    }
 
     public Celda[][] jugar(int row, int col, String tipoPiedra) {
-        return Gomoku.getInstance().getModoDeJuego().jugar(row, col, "PiedraPesada");
+        Celda[][] celdasAdyacentes = new Celda[3][3];
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i != 0 || j != 0) {
+                    celdasAdyacentes[i + 1][j + 1] = getBoard()[row + i][col + j];
+                }
+            }
+        }
+        for (Celda[] fila : celdasAdyacentes) {
+            for (Celda celda : fila) {
+                if (celda instanceof Ocupada) {
+                    return null;
+                }
+            }
+        }
+
+        return getBoard();
+
     }
+
+
 }
