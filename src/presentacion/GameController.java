d42cc5f7e25b24cd240832e846f8c308deb15a15
Diego <58009401+marzo245@users.noreply.github.com>
2023-12-12 14:52:17 -0500

    arreglo

--- src/presentacion/GameController.java
+++ src/presentacion/GameController.java
@@ -35,11 +35,6 @@ public class GameController implements ActionListener {
 	public final static int[] DELTA_Y = { -1, 1, 1, -1, 1, -1, 0, 0 };
 	public final static int NUMBER_DIRECTION = 8;
 
-	private final static ComputerPlayer computerPlayer = new ComputerPlayer();
-	private static boolean isPlayWithComputer = false;
-	private static boolean isPlayOnlyComputer = false;
-	private static boolean winnerFound = false;
-
 	/**
 	 * 
 	 * Constructor de la clase GameController.
@@ -108,7 +103,6 @@ public class GameController implements ActionListener {
 						"Evento", JOptionPane.INFORMATION_MESSAGE);
 				Gomoku.getInstance().setHayMensjae(false);
 			}
-			System.out.println("Anterior turno: " + Gomoku.getTurno());
 			Gomoku.getInstance().imprimirTablero();
 		} else {
 			JOptionPane.showMessageDialog(null,
@@ -131,80 +125,18 @@ public class GameController implements ActionListener {
 	 *
 	 */
 	public void checkWinner(int row, int col) {
-		for (int i = 0; i < NUMBER_DIRECTION; i += 2) {
-			// La coordenada en sí ya tiene una piedra.
-			int counter = 1;
-			rowOfWin.clear();
-			colOfWin.clear();
-			rowOfWin.add(row);
-			colOfWin.add(col);
-
-			counter += countColor(row + DELTA_X[i], col + DELTA_Y[i], i);
-			counter += countColor(row + DELTA_X[i + 1], col + DELTA_Y[i + 1],
-					i + 1);
-
-			if (counter >= 5) {
-				counter = 0;
-				for (int j = 0; j < rowOfWin.size(); j++) {
-					BoardComponent.getCells()[rowOfWin.get(j)][colOfWin.get(j)].setIsCellOfWin(true);
-				}
-				winnerFound = true;
-				cellDisableClick();
-				notifyWinner();
-				TimerComponent.getTimer().stop();
-				return;
+		if (Gomoku.getInstance().getSeEncontroGanador()) {
+			cellDisableClick();
+			cell.setIsCellOfWin(true);
+			notifyWinner();
+			TimerComponent.getTimer().stop();
+			int[][] win = Gomoku.getInstance().getPosicionesGanadoras();
+			for (int i = 0; i < 5; i++) {
+				BoardComponent.getCells()[win[0][i]][win[1][i]].setIsCellOfWin(true);
+				System.out.print(win[0][i] + " " + win[1][i] + " ");
 			}
+
 		}
-	}
-
-	/**
-	 * Método para contar la cantidad de piedras consecutivas en sentido horizontal,
-	 * vertical o diagonal, siempre y cuando las coordenadas verificadas aún estén
-	 * en el tablero y el color de la piedra en esas coordenadas sea del jugador
-	 * que está en su turno.
-	 * 
-	 * @param row
-	 *              índice de fila
-	 * @param col
-	 *              índice de columna
-	 * @param index
-	 *              índice para los arrays DELTA_X y DELTA_Y
-	 *
-	 */
-	public int countColor(int row, int col, int index) {
-		int counter = 0;
-
-		while (stillOnBoard(row, col)
-				&& BoardComponent.getCells()[row][col].getColor() == lastColor) {
-			counter++;
-			rowOfWin.add(row);
-			colOfWin.add(col);
-			row += DELTA_X[index];
-			col += DELTA_Y[index];
-			if (!stillOnBoard(row, col)) {
-				break;
-			}
-		}
-
-		return counter;
-	}
-
-	/**
-	 * Método para verificar si el índice dado todavía está dentro del tablero.
-	 * 
-	 * @param x
-	 *          índice de fila
-	 * @param y
-	 *          índice de columna
-	 * @return true si las coordenadas aún están dentro del tablero
-	 * @return false si no es así
-	 *
-	 */
-	public static boolean stillOnBoard(int x, int y) {
-		if (x >= 0 && x <= 18 && y >= 0 && y <= 18) {
-			return true;
-		}
-		return false;
 	}
 
 	/**
@@ -229,23 +161,11 @@ public class GameController implements ActionListener {
 	 * 
 	 */
 	public void notifyWinner() {
-		String winner = lastColor == Cell.BLACK ? "black" : "white";
-		String name = "";
-
-		if (winner.equals("black")) {
-			name = GomokuGUI.getFirstName();
-			ScoreComponent.updateScore(name, numStep);
-		}
-
-		else if (!isPlayWithComputer) {
-			name = GomokuGUI.getSecondName();
-			ScoreComponent.updateScore(name, numStep);
-		}
 		JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-		if (winner.equals("white") && isPlayWithComputer) {
-			info.setText("Ouch! Perdiste :(!");
+		if (Gomoku.getInstance().getGanador().equals("Negra")) {
+			info.setText("Bravo! " + GomokuGUI.getFirstName() + " ganó con " + numStep + " movimientos totales! :)");
 		} else {
-			info.setText("Bravo! " + name + " ganó con " + numStep + " movimientos totales! :)");
+			info.setText("Bravo! " + GomokuGUI.getSecondName() + " ganó con " + numStep + " movimientos totales! :");
 		}
 	}
 
@@ -258,42 +178,14 @@ public class GameController implements ActionListener {
 	 */
 	public static void restartGame() {
 		numStep = 0;
-		winnerFound = false;
 		rowOfWin.clear();
 		colOfWin.clear();
 		TimerComponent.getTimer().stop();
 		TimerComponent.resetTimer();
 		Gomoku.getInstance().crearBoard(3, 3);
 		GomokuGUI.getBoardComponent().clearBoard();
+		Gomoku.getInstance().setSeEncontroGanador(false);
 		GomokuGUI.getInfoComponent().clearInfo();
-	}
-
-	/*
-	 * metodo definir si se juega con la computadora o no
-	 */
-	public static void setIsPlayWithComputer(Boolean value) {
-		isPlayWithComputer = value;
-	}
-
-	/*
-	 * metodo definir si se juega solo con la computadora o no
-	 */
-	public static void setIsPlayOnliComputer(Boolean value) {
-		isPlayOnlyComputer = value;
-	}
-
-	/*
-	 * metodo para obtener el valor de la variable isPlayWithComputer
-	 */
-	public static boolean getIsPlayWithComputer() {
-		return isPlayWithComputer;
-	}
-
-	/*
-	 * metodo para obtener el valor de la variable isPlayOnlyComputer
-	 */
-	public static boolean getPlayOnliComputer() {
-		return isPlayOnlyComputer;
 	}
 
 	/*
@@ -323,135 +215,6 @@ public class GameController implements ActionListener {
 					"Draw, all cells are filled.",
 					"Draw", JOptionPane.INFORMATION_MESSAGE);
 		}
-	}
-
-	private void handleEmptyCell() {
-		numStep++;
-
-		if (lastColor == Cell.BLACK) {
-			cell.setColor(Cell.WHITE);
-			GomokuGUI.getBoardComponent().repaint();
-			lastColor = Cell.WHITE;
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + GomokuGUI.getFirstName()
-					+ " |   Color ficha: Negra  | Total movimientos: "
-					+ numStep);
-		} else {
-			cell.setColor(Cell.BLACK);
-			GomokuGUI.getBoardComponent().repaint();
-			lastColor = Cell.BLACK;
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + GomokuGUI.getSecondName()
-					+ " |   Color ficha: Blanca | Total movimientos: "
-					+ numStep);
-		}
-
-		checkWinner(cell.getRow(), cell.getCol());
-		checkCellAvailability();
-
-		if (!winnerFound && isPlayWithComputer) {
-
-			numStep++;
-
-			computerPlayer.play();
-			lastColor = Cell.WHITE;
-
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + GomokuGUI.getFirstName()
-					+ " | Color ficha: Negra | Total movimientos: "
-					+ numStep);
-
-			checkWinner(computerPlayer.getRow(), computerPlayer.getCol());
-			checkCellAvailability();
-		}
-	}
-
-	private void handleTeleportCell() {
-		int randomRow, randomCol;
-		do {
-			randomRow = (int) (Math.random() * (Board.HEIGHT - 1));
-			randomCol = (int) (Math.random() * (Board.WIDTH - 1));
-		} while (BoardComponent.getCells()[randomRow][randomCol].getColor() != Cell.EMPTY);
-
-		numStep++;
-
-		if (lastColor == Cell.BLACK) {
-
-			cell.setColor(Cell.EMPTY);
-			BoardComponent.getCells()[randomRow][randomCol].setColor(Cell.WHITE);
-
-			GomokuGUI.getBoardComponent().repaint();
-			lastColor = Cell.WHITE;
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + Gomoku.getPlayer1().getName()
-					+ " |   Color ficha: Negra  | Total movimientos: "
-					+ numStep);
-		}else {
-			cell.setColor(Cell.EMPTY);
-			BoardComponent.getCells()[randomRow][randomCol].setColor(Cell.BLACK);
-			GomokuGUI.getBoardComponent().repaint();
-			lastColor = Cell.BLACK;
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + Gomoku.getPlayer2().getName()
-					+ " |   Color ficha: Negra | Total movimientos: "
-					+ numStep);
-		}
-		
-		checkWinner(randomRow, randomCol);
-		checkCellAvailability();
-
-		if (!winnerFound && isPlayWithComputer) {
-			numStep++;
-			computerPlayer.play();
-			lastColor = Cell.WHITE;
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + GomokuGUI.getFirstName()
-					+ " | Color ficha: Negra | Total movimientos: "
-					+ numStep);
-					
-			checkWinner(computerPlayer.getRow(), computerPlayer.getCol());
-			checkCellAvailability();
-		}
-
-	}
-
-	private void handleMineCell() {
-		for (int i = Math.max(0, cell.getRow() - 1); i <= Math.min(Board.HEIGHT - 1, cell.getRow() + 1); i++) {
-			for (int j = Math.max(0, cell.getCol() - 1); j <= Math.min(Board.WIDTH - 1, cell.getCol() + 1); j++) {
-				BoardComponent.getCells()[i][j].setColor(Cell.EMPTY);
-				BoardComponent.getCells()[i][j].setEnableClick(true);
-			}
-		}
-		if (lastColor == Cell.BLACK) {
-			lastColor = Cell.WHITE;
-			cell.setColor(Cell.WHITE);
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + GomokuGUI.getFirstName()
-					+ " |   Color ficha: Negra  | Total movimientos: "
-					+ numStep);
-		} else {
-			lastColor = Cell.BLACK;
-			cell.setColor(Cell.BLACK);
-			JLabel info = GomokuGUI.getInfoComponent().getCurrentPlayer();
-			info.setText("Turno: " + GomokuGUI.getSecondName()
-					+ " |   Color ficha: Blanca | Total movimientos: "
-					+ numStep);
-		}
-
-		GomokuGUI.getBoardComponent().repaint();
-		JOptionPane.showMessageDialog(null,
-				"¡Has activado la celda de Mina!\n"
-						+ "Todas las celdas adyacentes han sido eliminadas.",
-				"Celda de Mina", JOptionPane.INFORMATION_MESSAGE);
-
-	}
-
-	private void handleGoldenCell() {
-		GomokuGUI.getBoardComponent().repaint();
-		JOptionPane.showMessageDialog(null,
-				"¡Has activado la celda de Golden!\n"
-						+ ".",
-				"Celda de Mina", JOptionPane.INFORMATION_MESSAGE);
 	}
 
 }
