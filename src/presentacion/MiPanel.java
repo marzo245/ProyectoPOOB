25036a092b867d050c59bbba1a976e1e621ea20b
Diego <58009401+marzo245@users.noreply.github.com>
2023-12-14 20:56:00 -0500

    arreglo titulo

--- src/presentacion/MiPanel.java
+++ src/presentacion/MiPanel.java
@@ -1,19 +1,31 @@
-import javax.swing.*;
-import java.awt.*;
+package presentacion;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 public class MiPanel extends JPanel {
-    private Image imagenDeFondo;
 
     public MiPanel() {
-        try {
-            imagenDeFondo = javax.imageio.ImageIO.read(new java.net.URL("http://ruta/a/tu/imagen.jpg"));
-        } catch (Exception e) {
-            /* Manejar error */ }
+        setBackground(Color.BLACK);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(imagenDeFondo, 0, 0, this);
+        BufferedImage gomokuFondo = null;
+        try {
+            gomokuFondo = ImageIO.read(new File("tituloDelJUEGO.png"));
+            System.out.println("Se cargo la imagen");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        g.drawImage(gomokuFondo, 0, 0, 800, 150, null);
     }
 }
